e76897cf8b070a315a2013a0f33a802a9f46abff
Geovani Gomes <geovanny.1gomes@gmail.com>
2024-02-20 00:07:47 -0300

    finish task

--- node/index.js
+++ node/index.js
@@ -10,9 +10,10 @@ create_table();
 app.get('/', (req, res) => {
     insert_data();
     get_all((names) => {
-        let response = '<h1>Full Cycle</h1><br> Nomes registrados:<br>';
-
+        let response = '<h1>Full Cycle</h1><br> Faça requisições para gravar os nomes:<br>';
         if (names != null){
+            
+            response = '<h1>Full Cycle</h1><br> Nomes registrados:<br>';
             names.forEach((item) => {
                 response += `ID: ${item.id}, Nome: ${item.name}<br>`;
             });
